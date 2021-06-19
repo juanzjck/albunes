501fe101f71d2435053e3b68e3ba9ea617239be5
JuanPa Salazar <juanzjck1996@gmail.com>
2021-06-19 12:13:48 -0500

    init

--- app/src/main/java/com/example/albun/AlbunInfo.java
+++ app/src/main/java/com/example/albun/AlbunInfo.java
@@ -1,6 +1,9 @@
 package com.example.albun;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,5 +14,18 @@ public class AlbunInfo extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.albun_info);
+
+        String id=(String) getIntent().getSerializableExtra("ID");
+        String name=(String) getIntent().getSerializableExtra("NAME");
+
+        TextView textView = findViewById(R.id.nameAlbun);
+        textView.setText(name);
+
+    }
+
+    public void newImage(View v){
+        Intent intent = new Intent(getApplicationContext(),FotoInfo.class);
+
+        startActivity(intent);
     }
 }
